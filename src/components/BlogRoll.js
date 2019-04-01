057a86fd537777ef8bd518f87b8fef1f86ba2b74
Dave Myburgh <davemyb@gmail.com>
2019-03-31 22:09:39 -0400

    Started stripping out the unnecessary stuff.

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -8,33 +8,33 @@ class BlogRoll extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <div className="columns is-multiline">
+      <div className="article">
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article className="tile is-child box notification">
-                <p>
-                  <Link
-                    className="title has-text-primary is-size-4"
-                    to={post.fields.slug}
-                  >
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <span className="subtitle is-size-5 is-block">
-                    {post.frontmatter.date}
-                  </span>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
+            <article>
+              <p>
+                <Link
+                  className="title"
+                  to={post.fields.slug}
+                >
+                  {post.frontmatter.title}
+                </Link>
+                <span> &bull; </span>
+                <span className="subtitle">
+                  {post.frontmatter.date}
+                </span>
+              </p>
+              <p>
+                {post.excerpt}
+                <br />
+                <br />
+                <Link className="button" to={post.fields.slug}>
+                  Keep Reading →
+                </Link>
+                <br />
+                <br />
+              </p>
+            </article>
           ))}
       </div>
     )
