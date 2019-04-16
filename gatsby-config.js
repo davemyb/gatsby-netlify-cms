180a3d5940924c6b71d978e59f13f871ac549976
Dave Myburgh <davemyb@gmail.com>
2019-04-15 22:05:56 -0400

    Tweak site settings and layout.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'DaveMyburgh.com',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'My stumbling attempts at building a decoupled website in markdown with Gatsby and Netlify.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
